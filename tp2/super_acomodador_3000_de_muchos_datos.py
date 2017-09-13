1828869ce105d9753db609e39f602353138a7b94
Gifaro <francisco.figari@hotmail.com>
2017-09-13 17:03:43 -0300

    Ahora reacomoda paciente por paciente porque si no vuela todo por los aires

--- tp2/super_acomodador_3000_de_muchos_datos.py
+++ tp2/super_acomodador_3000_de_muchos_datos.py
@@ -1,17 +1,17 @@
 import scipy.io
 import pandas as pd
-import seaborn as sns
 import numpy as np
 import gc
 
 
-N_pacientes = 1
-path = "/home/francisco/tps/datos/tp2/~fraimondo/cienciadatos/data/P0{}.mat"
-dfs = []
+N_pacientes = 4
+path = "/media/libre/los_datos_de_datos/calamaro.exp.dc.uba.ar/~fraimondo/cienciadatos/data/"
+paciente_path = path + "P0{}.mat"
+save_path = path + "dataframe_reconstruido_{}.pickle"
 for paciente in range(1, N_pacientes + 1):
     print("Paciente {}".format(paciente))
     print("Levantando matriz...")
-    mat = scipy.io.loadmat(path.format(paciente))['data']
+    mat = scipy.io.loadmat(paciente_path.format(paciente))['data']
     print("Listo")
 
     N_epochs = mat.shape[0]
@@ -20,6 +20,7 @@ for paciente in range(1, N_pacientes + 1):
 
     N = N_epochs * N_sensores * N_valores
 
+    print("Creando index...")
     iterables = [
         [paciente for _ in range(N)] ,
         [i for i in range(N_epochs) for j in range(N_sensores) for k in range(N_valores)],
@@ -27,7 +28,6 @@ for paciente in range(1, N_pacientes + 1):
         [k for i in range(N_epochs) for j in range(N_sensores) for k in range(N_valores)],
     ]
 
-    print("Creando index...")
     index = pd.MultiIndex.from_arrays(iterables, names=["paciente", "epoch", "sensor", "tiempo"])
     print("Listo")
     
@@ -36,15 +36,12 @@ for paciente in range(1, N_pacientes + 1):
     print("Listo")
 
     print("Creando DataFrame...")
-    dfs.append(pd.DataFrame({"valores": reshaped_mat}, index=index))
+    df_paciente = pd.DataFrame({"valores": reshaped_mat}, index=index)
     print("Listo")
-
-    gc.collect()
+   
+    print("Guardando pickle...")
+    df_paciente.to_pickle(save_path.format(paciente))
+    print("Listo")
     
-print("Concatenando dataframes...")
-final_df = pd.concat(dfs)
-print("Listo")
-print("Guardando dataframe en formato hdf...")
-final_df.to_hdf('dataframe_reconstruido.hdf', 'mis_datos', format='table', mode='w')
-# Se levanta con pd.read_hdf('dataframe_reconstruido.hdf')
-print("Listo")
+    gc.collect()
+
